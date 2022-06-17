c6fd7544985e698309059a59d09a5ce2e188ae84
Moon(Dan) <danielodom23@gmail.com>
2022-06-17 10:34:17 -0400

    Add Class modification to script.js

--- DOM Element Manipulation/script.js
+++ DOM Element Manipulation/script.js
@@ -48,77 +48,98 @@ const InnerHtml = (content = "<h2>Inner Html</h2>", el = "div") => {
 
 // a) using remove
 const Remove = () => {
-    const element = document.createElement('div')
-    element.innerText = 'Remove me'
-    body.append(element)
-    element.remove()
-}
+  const element = document.createElement("div");
+  element.innerText = "Remove me";
+  body.append(element);
+  element.remove();
+};
 
 // b) using removeChild
 const RemoveChild = () => {
-    const element = document.createElement('div')
-    element.innerText = 'Remove me'
-    body.append(element)
-    body.removeChild(element)
-}
+  const element = document.createElement("div");
+  element.innerText = "Remove me";
+  body.append(element);
+  body.removeChild(element);
+};
 
 // Add/Edit Element Attributes (e.g. title, id, class, ...)
 
 // a) Using set attribute
-const SetAttribute  = (atr = 'id', value = 'set-attribute') => {
-    const element = document.createElement('h2')
-    element.innerText = 'Set Attribute'
-    element.setAttribute(atr, value)
-    body.append(element)
-} 
+const SetAttribute = (atr = "id", value = "set-attribute") => {
+  const element = document.createElement("h2");
+  element.innerText = "Set Attribute";
+  element.setAttribute(atr, value);
+  body.append(element);
+};
 
 // b) Using explicit method
-const Attribute = (value = 'attribute') => {
-    const element = document.createElement('h2')
-    element.innerText = 'Attribute'
-    element.id = value
-    body.append(element)
-}
+const Attribute = (value = "attribute") => {
+  const element = document.createElement("h2");
+  element.innerText = "Attribute";
+  element.id = value;
+  body.append(element);
+};
 
 // Remove Element Attributes (e.g. title, id, class, ...)
 
 const RemoveAttribute = () => {
-    const element = document.createElement('h2')
-    element.innerText = 'Remove Attribute'
-    element.id = 'remove-attribute'
-    body.append(element)
-    element.removeAttribute('id')
-}
-
+  const element = document.createElement("h2");
+  element.innerText = "Remove Attribute";
+  element.id = "remove-attribute";
+  body.append(element);
+  element.removeAttribute("id");
+};
 
 // Add/Edit data attributes
 
-const DataAttribute = (value = 'data-attribute') => {
-    const element = document.createElement('h2')
-    element.innerText = 'Add Data Attribute'
-    body.append(element)
-    // dataset is *important*
-    element.dataset.add = value
-}
+const DataAttribute = (value = "data-attribute") => {
+  const element = document.createElement("h2");
+  element.innerText = "Add Data Attribute";
+  body.append(element);
+  // dataset is *important*
+  element.dataset.add = value;
+};
 
 // Read data attribute
 
-const ReadDataAttribute = (value = 'data-attribute') => {
-    const element = document.createElement('h2')
-    element.innerText = 'Before Read'
-    body.append(element)
-    element.dataset.read = value
-    element.innerText = element.dataset.read
-}
+const ReadDataAttribute = (value = "data-attribute") => {
+  const element = document.createElement("h2");
+  element.innerText = "Before Read";
+  body.append(element);
+  element.dataset.read = value;
+  element.innerText = element.dataset.read;
+};
 
-Append()
-AppendChild()
-TextContent()
-InnerHtml()
-Remove()
-RemoveChild()
-SetAttribute()
-Attribute()
-RemoveAttribute()
-DataAttribute()
-ReadDataAttribute()
+// Modify Class Properties
+
+// Add class
+const ClassAdd = () => {
+  const element = document.createElement("h2");
+  element.innerText = "Add Class";
+  body.append(element);
+  element.classList.add("add-class");
+};
+
+// Remove Class
+
+const ClassRemove = () => {
+  const element = document.createElement("h2");
+  element.innerText = "Remove Class";
+  element.classList.add("add-class");
+  body.append(element);
+  element.classList.remove("add-class");
+};
+
+Append();
+AppendChild();
+TextContent();
+InnerHtml();
+Remove();
+RemoveChild();
+SetAttribute();
+Attribute();
+RemoveAttribute();
+DataAttribute();
+ReadDataAttribute();
+ClassAdd();
+ClassRemove()
